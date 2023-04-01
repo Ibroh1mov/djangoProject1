91b65363287b1d182f5af28c9dca3407284e6fbf
GrayWizard12345 <muslim4ik14@gmail.com>
2023-04-01 12:01:02 +0500

    init

--- poll/models.py
+++ poll/models.py
@@ -11,3 +11,5 @@ class Choice(models.Model):
         Question,
         on_delete=models.CASCADE
     )
+    choice_text = models.CharField(max_length=256)
+    votes = models.IntegerField(default=0)
--- poll/views.py
+++ poll/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Question
 # Create your views here.
+def get_question(request, question_id):
+    question_text = Question.objects.get(id=question_id)
+    reply = f"""Here is question number {question_id}:
+    <br>{question_text}"""
+    return HttpResponse(reply)
